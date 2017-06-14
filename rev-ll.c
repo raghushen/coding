7d9ec8c3b20f75e397404a5fd3d2c96211fbe330
raghushen <ragu.shen@gmail.com>
2017-06-14 07:12:00 -0700

    Added recursive fn. to reverse doubly linked list

--- rev-ll.c
+++ rev-ll.c
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct dnode {
+	int val;
+	struct dnode *next;
+	struct dnode *prev;
+} Dnode;
+
 typedef struct node {
 	int val;
 	struct node *next;
@@ -13,9 +19,15 @@ typedef struct node {
 Node *
 rec_rev(Node *head)
 {
-	if (head == NULL) return (head);
-	head->next = rec_rev(head->next);
-	
+	Node *r;
+
+	if (head == NULL || head->next == NULL) return (head);
+
+	r = rec_rev(head->next);
+	head->next->next = NULL;
+	head->next = NULL;
+
+	return (r);
 }
 
 Node *
@@ -27,6 +39,15 @@ create_node(int val) {
 
 }
 
+Dnode *
+create_dnode(int val) {
+	Dnode *node = (Dnode *)malloc(sizeof(Dnode));
+	node->val = val;
+	node->next = NULL;
+	node->prev = NULL;
+
+	return (node);
+}
 
 void
 create_list(Node **head, int val)
@@ -42,6 +63,36 @@ create_list(Node **head, int val)
 		tmp = tmp->next;
 
 	tmp->next = create_node(val);
+}
+
+void
+create_dlist(Dnode **head, int val)
+{
+	Dnode *tmp;
+
+	if (*head == NULL) {
+		*head = create_dnode(val);
+		return ;
+	}
+	tmp = *head;
+	while(tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = create_dnode(val);
+	tmp->next->prev = tmp;
+}
+
+Node *
+rev_list(Node *h) {
+
+	Node *r;
+
+	if (h==NULL || h->next==NULL) return (h);
+
+	r = rev_list(h->next);
+	h->next->next = h;
+	h->next = NULL;
+	return (r);
 }
 
 Node *
@@ -61,16 +112,22 @@ rev_recursive(Node **head, Node *node)
 	return (node);
 }
 
-Node *
-rev_rec_list(Node *head) {
-	Node *t;
-	/* NB: This return(head) don't changed as reccursion unwinds. Hence, last node is eventually returned as head */
-	if (head == NULL || head->next == NULL) return (head);
-	
-	t = rev_rev_list(head->next);
-	head->next->next = h;
+/* Reverse a doubly linked list */
+Dnode *
+rev_dlist(Dnode *head) {
+	Dnode *r;
+
+	if (head == NULL) return (head);
+	if (head->next == NULL) {
+		head->prev = NULL; // Needed?
+		return (head);
+	}
+	r = rev_dlist(head->next);
+	head->next->next = head;
+	head->prev = head->next;
 	head->next = NULL;
-	return(t); /* NB: same node pointer gets returned as it unwinds */
+
+	return (r);
 }
 
 void
@@ -84,24 +141,64 @@ print_list(Node *head)
 	}
 }
 
+void
+print_dlist(Dnode *head)
+{
+	Dnode *tmp = head;
+	printf("\nList:  ");
+	while (tmp != NULL) {
+		printf("%d ", tmp->val);
+		tmp = tmp->next;
+	}
+}
+
 int
 main()
 {
 	Node *head = NULL;
+	Node *tmp = NULL;
+
+	Dnode *dhead = NULL;
+	Dnode *dtmp = NULL;
+	
+	/* Create single and double linked lists */
+
 	create_list(&head, 1);
+	create_dlist(&dhead, 1);
+
 	create_list(&head, 2);
+	create_dlist(&dhead, 2);
+
 	create_list(&head, 3);
+	create_dlist(&dhead, 3);
+
 	create_list(&head, 4);
+	create_dlist(&dhead, 4);
+
 	create_list(&head, 5);
+	create_dlist(&dhead, 5);
+
 	create_list(&head, 6);
+	create_dlist(&dhead, 5);
+
 	create_list(&head, 7);
+	create_dlist(&dhead, 7);
+
 	create_list(&head, 8);
+	create_dlist(&dhead, 8);
+
 	create_list(&head, 9);
-	print_list(head);
+	create_dlist(&dhead, 9);
+
 	// rev_recursive(&head, head);
 	// print_list(head);
-	// recursive(&head, head);
-	// print_list(head);
-	head = rev_rec_list(head);
+	// rev_recursive(&head, head);
+	printf("\nRev. ");
+	head = rev_list(head);
 	print_list(head);
+
+	printf("\nDlist:");
+	print_dlist(dhead);
+	dhead = rev_dlist(dhead);
+	print_dlist(dhead);
 }
